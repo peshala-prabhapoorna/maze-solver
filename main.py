e6034b0ea6f656cc54e73c1be70c958d8e272883
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-09-05 22:43:53 +0530

    add Maze class

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-from cell import Cell
 from graphics import Window
+from maze import Maze
 
 
 def main():
     win = Window(800, 600)
 
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
-
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
-    c2.draw_move(c1, True)
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
-    c3.draw_move(c2, True)
-
-    c4 = Cell(win)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
-    c4.draw_move(c3)
+    Maze(50, 50, 10, 14, 50, 50, win)
 
     win.wait_for_close()
 
